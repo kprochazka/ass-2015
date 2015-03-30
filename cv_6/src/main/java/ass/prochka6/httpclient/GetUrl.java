6b6e6cc75db8e3745246620966553d3d484848a9
Prochazka Kamil <Kamil.Prochazka@airbank.cz>
2015-03-31 01:05:23 +0200

    Lesson 6 - DU2 - improved version

--- cv_6/src/main/java/ass/prochka6/httpclient/GetUrl.java
+++ cv_6/src/main/java/ass/prochka6/httpclient/GetUrl.java
@@ -1,14 +1,13 @@
 package ass.prochka6.httpclient;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * @author Kamil Prochazka
@@ -16,16 +15,11 @@ import java.io.InputStreamReader;
 public class GetUrl {
 
     public static void main(String[] args) throws IOException {
-        HttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
-
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
         HttpResponse response = httpClient.execute(new HttpGet("https://www.random.org/strings/?num=10&len=10&digits=on&unique=on&format=html&rnd=new"));
-
-        try (BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));) {
-            String line = null;
-            while ((line = rd.readLine()) != null) {
-                System.out.println(line);
-            }
-        }
+        System.out.println(EntityUtils.toString(response.getEntity(), EntityUtils.getContentCharSet(response.getEntity())));
+        EntityUtils.consume(response.getEntity());
+        httpClient.close();
     }
 
 }
