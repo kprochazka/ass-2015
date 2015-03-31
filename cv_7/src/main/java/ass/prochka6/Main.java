3fc6b8f1a79e8fb7f090f1b1de520bd3d9870754
Prochazka Kamil <Kamil.Prochazka@airbank.cz>
2015-03-31 15:03:47 +0200

    Lesson 7 - lesson assignment

--- cv_7/src/main/java/ass/prochka6/Main.java
+++ cv_7/src/main/java/ass/prochka6/Main.java
@@ -2,7 +2,12 @@ package ass.prochka6;
 
 import de.odysseus.el.util.SimpleContext;
 
+import javax.el.ArrayELResolver;
+import javax.el.BeanELResolver;
+import javax.el.CompositeELResolver;
 import javax.el.ExpressionFactory;
+import javax.el.ListELResolver;
+import javax.el.MapELResolver;
 import javax.el.ValueExpression;
 
 /**
@@ -12,8 +17,19 @@ public class Main {
 
     public static void main(String[] args) {
         ExpressionFactory expressionFactory = ExpressionFactory.newInstance();
+
         SimpleContext context = new SimpleContext();
-        ValueExpression ve = expressionFactory.createValueExpression(context, "${5*(60-10)}", Integer.class);
+        CompositeELResolver compositeELResolver = new CompositeELResolver();
+        compositeELResolver.add(new ArrayELResolver());
+        compositeELResolver.add(new ListELResolver());
+        compositeELResolver.add(new MapELResolver());
+        compositeELResolver.add(new BeanELResolver());
+        context.setELResolver(compositeELResolver);
+
+        // assign mapping of person to context
+        context.getVariableMapper().setVariable("p", expressionFactory.createValueExpression(new Person("Kamil"), Person.class));
+
+        ValueExpression ve = expressionFactory.createValueExpression(context, "${p.id.concat('a')}", String.class);
         Object value = ve.getValue(context);
         System.out.println(value);
     }
---
+++ cv_7/src/main/java/ass/prochka6/Person.java
@@ -0,0 +1,21 @@
+package ass.prochka6;
+
+/**
+ * @author Kamil Prochazka
+ */
+public class Person {
+
+    private String id;
+
+    public Person(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
